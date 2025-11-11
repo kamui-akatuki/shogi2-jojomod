2a236a09e0909706398f3c0cb070f33324289990
kamui-akatuki <kamui-akatuki@kamui-akatukinoMacBook-Air.local>
2025-11-11 16:41:29 +0900

    ミスってnode_modules入れちゃった(っ◞‸◟c)

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("hello");
+import { Event, GameData, ModBase, Player, Request, RequestUpdater, StartEvent } from "shogi2-types";
+
+export default class Jojo extends ModBase{
+  onStart(_data: GameData, _event: StartEvent, _sender: Player, _updater: RequestUpdater): { r: Request[]; e: Event[]; } {
+    console.log("jojo mod is loaded!");
+    return {r:[],e:[]};
+  }
+};
