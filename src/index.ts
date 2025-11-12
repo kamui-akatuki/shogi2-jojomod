cc8a010a01b395f38d1ed7337252eca16ac4af95
kamui-akatuki <kamui-akatuki@kamui-akatukinoMacBook-Air.local>
2025-11-12 09:50:52 +0900

    a

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import { Event, GameData, ModBase, MoveEvent, Player, Request, RequestUpdater, StartEvent } from "shogi2-types";
+import { Event, GameData, ModBase, Player, Request, RequestUpdater, StartEvent } from "shogi2-types";
 
 export default class Jojo extends ModBase{
   onStart(_data: GameData, _event: StartEvent, _sender: Player, _updater: RequestUpdater): { r: Request[]; e: Event[]; } {
     console.log("jojo mod is loaded!");
-    return {r:[],e:[]};
-  }
-  onMove(_data: GameData, _event: MoveEvent, _sender: Player, _updater: RequestUpdater): { r: Request[]; e: Event[]; } {
-    console.log("だが断る");
     return {r:[],e:[]};
   }
 };
