e70e73691e27a3591ec0d89374d71328d4b71254
kamui-akatuki <kamui-akatuki@PC076.std.EE.kurume-nct.ac.jp>
2025-11-12 16:09:52 +0900

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { BoardRequest, createBoard,  Event,  GameData, ModBase, Player, RequestExpansion, RequestUpdater, StartEvent } from "shogi2-types";
 import {cloneDeep} from "lodash";
+import set_pieces from "./set_pieces";
 
 export default class Jojo extends ModBase{
   onStart(d: GameData, _before: GameData, _event: StartEvent, _sender: Player, _updater: RequestUpdater): { r: RequestExpansion[]; e: Event[]; } {
     const data=cloneDeep(d);
     const board=createBoard(15,15);
+    set_pieces(board);
     data.board=board;
     const request=new BoardRequest("both",board,"exclude");
     return {r:[{request,data}],e:[]};
